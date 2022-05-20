d24c8005b3bbb5960f047f0d84e095f65c18c3ea
JamesCollyer <collyerfj@gmail.com>
2022-05-20 16:44:44 +1000

    TODO: video and evidence

--- app.r
+++ app.r
@@ -5,33 +5,65 @@ library(tidyverse)
 library(scales)
 library(dplyr)
 library(sigmoid)
+library(ggthemes)
 
 source("model.r")
 
-ui <- fluidPage(theme = shinytheme("flatly"),
+ui <- fluidPage(theme = shinytheme("cosmo"),
+                # in-line css styling
+                tags$style(
+                  "
+                  body {
+	                  background-image: linear-gradient(to top, #096979 0%, #616294
+		                  100%);
+                  }
+                  .top_1 {
+                    color: Black;
+                    font-size: 30px;
+                  }
+                  .top_2{
+                  color: White;
+                  font-size: 30px;
+                  }
+                  .text_normal{
+                    font-size: 20px;
+                    color: Black;
+                  }
+                  "
+                ),
+                
                 navbarPage(
-                  theme = "cyborg",
+                  theme = "darkly",
                   "Project 3",
-                  tabPanel("Video Introduction",
+                  tabPanel("Introduction",
                            sidebarPanel(
-                            
+                             p(class = "top_1", "Company Description"),
+                             p(class = "text_normal", "Third party company that specializes in making fligh plan recommendations to travelers.
+                               They wish to expand into the internett provider recommendation space."),
+                             p(class = "top_1", "Recommendation"),
+                             p(class = "text_normal", "The provided tool allows for a visualy appealing ranking of the providers in the customers area.
+                               The tool can be directly provided for customer use, or the information it provides may serve as the foundation of a recommendation.
+                               The rankings are based on three factors; upload-speed, download-speed and latency.
+                               It also allows for the user to input an importance weighting for each of these that will be reflected in the ranking."),
+                             width = 3
+                             
                            ),
                            
                            mainPanel(
+                             p(class = "top_2", "Introduction Video:"),
                              tabsetPanel(type = "tab",
-                                         tabPanel(HTML('<iframe width="750" height="500" src="https://www.youtube.com/embed/fJXzIU4vmuA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'))
+                                         tabPanel(HTML('<iframe width="999" height="666" src="https://www.youtube.com/embed/fJXzIU4vmuA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'))
                              )
-                             
                            )
                   ),
                   
                   tabPanel("Recommendation Tool",
                            sidebarPanel(
-                             HTML('<h4>Options'),
+                             p(class = "top_1", "Options:"),
                              
-                             selectInput("state_in", "State:", c("NSW", "VIC", "QLD", "NT + SA", "ACT", "WA", "TAS")),
+                             selectInput("state_in", "State:", c("All", "NSW", "VIC", "QLD", "NT + SA", "ACT", "WA", "TAS")),
                              
-                             selectInput("tier_in", "Tier:", c("12/1 Mbps", "25/5 Mbps", "25/10 Mbps", "50/20 Mbps", "75/20 Mbps", "100/40 Mbps", "250/25 Mbps", "500-999/50 Mbps", "")),
+                             selectInput("tier_in", "Tier:", c("All", "12/1 Mbps", "25/5 Mbps", "25/10 Mbps", "50/20 Mbps", "75/20 Mbps", "100/20 Mbps", "100/40 Mbps", "250/25 Mbps")),
                              
                              sliderInput("up_pref_in", "Level of concern for upload speed:", value = 1, min = 0, max = 1),
                              
@@ -39,24 +71,37 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                              
                              sliderInput("lat_pref_in", "Level of concern for latency:", value = 1, min = 0, max = 1),
                              
-                             actionButton("submittion", "Submit", class = "btn btn-primary")
+                             actionButton("submittion", "Submit", class = "btn btn-primary"),
+                             
+                             width = 3
                            ),
                           
                            mainPanel(
-                             tags$label(h3("Provider Comparison:")),
+                             p(class = "top_2", "Ranking/Recommendation:"),
                              verbatimTextOutput("result"),
                              
-                             plotOutput("comparison")
+                             plotOutput("comparison"),
+                             width = 9
                              
                            )
                   ),
                   
                   
                   tabPanel("Evidence",
-                           mainPanel(
-                             
+                           sidebarPanel(
+                             p(class = "top_1", "Explanation/Evidence"),
+                             p(class = "text_normal", "The calculations are based on soandso.
+                               they are made soandso.
+                               as time passes the tool becomes less valid/applicable.
+                               Some cat less inputs"),
+                             width = 4
                            
-                           )),
+                           ),
+                           
+                           mainPanel(
+                             p(class = "top_2", "Plot without using the sigmoid function. State: All, Tier: All, Level of concern: 1"),
+                             plotOutput("evidence"), width = 8
+                           ))
                 )
 )
 
@@ -73,18 +118,23 @@ server <- function(input, output, session) {
   # Text box
   output$result <- renderPrint({
     if(input$submittion>0){
-      isolate("Calculation complete, best provider has the highest score")
+      isolate("Calculation complete, best provider has the highest score. Score of 0 is an indication that there was no available data.")
     }
     else{
       return("Ready to make recommendation")
     }
   })
   
-  # Display Plot
+  # Display Plot Ranking
   output$comparison <- renderPlot({
     if (input$submittion>0) { 
       isolate(UI_input())
     } 
+  })
+  
+  # Display Plot Evidence
+  output$evidence <- renderPlot({
+    evidence("All", "All", 1, 1, 1)
   })
   
 }
--- model.r
+++ model.r
@@ -1,6 +1,7 @@
 library(dplyr)
 library(ggplot2)
 library(sigmoid)
+library(ggthemes)
 
 # Function to make a recommendation based on user input
 recommend <- function(state, tier_pref, up_co, down_co, latency_co){
@@ -9,10 +10,22 @@ recommend <- function(state, tier_pref, up_co, down_co, latency_co){
   data_raw <- read.csv("http://www.maths.usyd.edu.au/u/UG/JM/DATA1001/r/current/projects/2022data/broadband.csv")
   
   # Cleaning Data
-  data = data_raw %>% filter(data_raw$rsp != "" & data_raw$rsp != "Other RSPs")
-
+  data = data_raw %>% filter(data_raw$rsp != "" & data_raw$rsp != "Other RSPs" & data_raw$is_this_service_impaired != "TRUE")
+  data = data %>% filter(data$tier != "Fixed Wireless Plus" & data$tier != "500-999/50 Mbps" & data$tier != "250/100 Mbps")
+  
   # Consider user location and requested tier
-  data = data %>% filter(data$state_or_territory == state & data$tier == tier_pref)
+  if(state == "All" & tier_pref == "All"){
+    data <- data
+  }
+  else if(state == "All"){
+    data = data %>% filter(data$tier == tier_pref)
+  }
+  else if(tier_pref == "All"){
+    data = data %>% filter(data$state_or_territory == state)
+  }
+  else{
+    data = data %>% filter(data$state_or_territory == state & data$tier == tier_pref)
+  }
 
   # Separate the providers for calculations
   optus = filter(data, data$rsp == "Optus")
@@ -28,7 +41,7 @@ recommend <- function(state, tier_pref, up_co, down_co, latency_co){
   providers_list = list(optus, tpg, dodo, iinet, exetel, aussie, vodafone, telstra, superloop)
   Providers = c("Optus", "TPG", "Dodo & iPrimus", "iiNet", "Exetel", "Aussie Broadband", "Vodafone", "Telstra", "Superloop")
     
-  # Calculate recommendation based on a combination of the rsp z score and user preference weighting
+  # Calculate recommendation based on a combination of the provider z score and user preference weighting
   z_up = 0
   z_down = 0
   z_lat = 0
@@ -61,12 +74,91 @@ recommend <- function(state, tier_pref, up_co, down_co, latency_co){
   
   # Converting to a data frame and printing a ranked bar graph
   df <- data.frame(Providers, providers_score)
-  p <- ggplot(df, aes(x=reorder(Providers, -providers_score), y=providers_score, fill = Providers)) +
+  p <- df %>% ggplot(aes(x=reorder(Providers, -providers_score), y=providers_score, fill = Providers)) +
     geom_bar(stat="identity") +
-    xlab("Providers") +
-    ylab("Score")
+    ylab("Score") +
+    xlab("") +
+    theme_gdocs(base_size = 14)
   p
 
+}
+
+# Function to produce evidence plot
+# This is simply a version of the recommend function without the sigmoid function
+evidence <- function(state, tier_pref, up_co, down_co, latency_co){
+  
+  # Importing Data
+  data_raw <- read.csv("http://www.maths.usyd.edu.au/u/UG/JM/DATA1001/r/current/projects/2022data/broadband.csv")
+  
+  # Cleaning Data
+  data = data_raw %>% filter(data_raw$rsp != "" & data_raw$rsp != "Other RSPs" & data_raw$is_this_service_impaired != "TRUE")
+  data = data %>% filter(data$tier != "Fixed Wireless Plus" & data$tier != "500-999/50 Mbps" & data$tier != "250/100 Mbps")
+  
+  # Consider user location and requested tier
+  if(state == "All" & tier_pref == "All"){
+    # No filter
+  }
+  else if(state == "All"){
+    data = data %>% filter(data$tier == tier_pref)
+  }
+  else if(tier_pref == "All"){
+    data = data %>% filter(data$state_or_territory == state)
+  }
+  else{
+    data = data %>% filter(data$state_or_territory == state & data$tier == tier_pref)
+  }
+  
+  # Separate the providers for calculations
+  optus = filter(data, data$rsp == "Optus")
+  tpg = filter(data, data$rsp == "TPG")
+  dodo = filter(data, data$rsp == "Dodo & iPrimus")
+  iinet = filter(data, data$rsp == "iiNet")
+  exetel = filter(data, data$rsp == "Exetel")
+  aussie = filter(data, data$rsp == "Aussie Broadband")
+  vodafone = filter(data, data$rsp == "Vodafone")
+  telstra = filter(data, data$rsp == "Telstra")
+  superloop = filter(data, data$rsp == "Superloop")
+  
+  providers_list = list(optus, tpg, dodo, iinet, exetel, aussie, vodafone, telstra, superloop)
+  Providers = c("Optus", "TPG", "Dodo & iPrimus", "iiNet", "Exetel", "Aussie Broadband", "Vodafone", "Telstra", "Superloop")
+  
+  # Calculate recommendation based on a combination of the provider z score and user preference weighting
+  z_up = 0
+  z_down = 0
+  z_lat = 0
+  providers_score = c()
+  current_score = 0
+  
+  for(i in providers_list){
+    
+    # If there are no data points from a provider in the given category they are excluded
+    if(dim(i)[1] == 0){
+      providers_score <- c(providers_score, 0)
+      next
+    }
+    
+    # We consider the Z scores for each category and multiply them by importance
+    z_up = (mean(i$All.hour.trimmed.mean.upload.speed) - mean(data$All.hour.trimmed.mean.upload.speed)) / sd(data$All.hour.trimmed.mean.upload.speed)
+    z_down = (mean(i$All.hour.trimmed.mean.download.speed) - mean(data$All.hour.trimmed.mean.download.speed)) / sd(data$All.hour.trimmed.mean.download.speed)
+    # Note inverse score for latency as lower is better
+    z_lat = -1 * ((mean(i$All.hour.trimmed.mean.latency) - mean(data$All.hour.trimmed.mean.latency)) / sd(data$All.hour.trimmed.mean.latency))
+    
+    current_score = (up_co * z_up) + (down_co * z_down) + (latency_co * z_lat)
+    
+    # Appending with each iteration
+    providers_score <- c(providers_score, current_score)
+    
+  }
+  
+  # Converting to a data frame and printing a ranked bar graph
+  df <- data.frame(Providers, providers_score)
+  p <- df %>% ggplot(aes(x=reorder(Providers, -providers_score), y=providers_score, fill = Providers)) +
+    geom_bar(stat="identity") +
+    ylab("Score") +
+    xlab("") +
+    theme_gdocs(base_size = 14)
+  p
+  
 }
 
 
